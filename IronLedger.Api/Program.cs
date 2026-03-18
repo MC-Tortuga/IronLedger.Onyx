bcce66cd3793e0f6018eb8838c6302d2d944f69e
malcolm <malcolmcarelse@gmail.com>
2026-03-18 23:12:26 +0200

    Load database connection string from .env file and auto-create DB on startup

--- IronLedger.Api/Program.cs
+++ IronLedger.Api/Program.cs
@@ -2,14 +2,25 @@ using IronLedger.Api.Endpoints;
 using IronLedger.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
+var envPath = Path.Combine(Directory.GetCurrentDirectory(), "..", ".env");
+var connectionString = File.ReadAllLines(envPath)
+    .First(l => l.StartsWith("ConnectionStrings__DefaultConnection="))
+    .Split('=', 2)[1];
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<OnyxDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OnyxDbContext>();
+    await db.Database.EnsureCreatedAsync();
+}
 
 if (app.Environment.IsDevelopment())
 {
